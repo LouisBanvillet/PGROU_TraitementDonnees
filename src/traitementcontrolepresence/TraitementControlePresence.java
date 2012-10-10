03d9045f7e23b544ffc95d45a00338e63d1f535e
LouisBanvillet <Louis@130.66.204.223>
2012-10-10 09:24:21 +0200

    Création du dossier traitement des données du projet de groupe sous GitHub

---
+++ src/traitementcontrolepresence/TraitementControlePresence.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package traitementcontrolepresence;
+
+import Frame.TraitementControlePresenceFrame;
+
+/**
+ *
+ * @author Louis
+ */
+public class TraitementControlePresence {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        TraitementControlePresenceFrame TraitementFrame = new TraitementControlePresenceFrame();
+        TraitementFrame.setVisible(true);
+    }
+}
