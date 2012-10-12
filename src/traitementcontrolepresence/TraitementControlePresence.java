3ac1e5e34928540887c11c20f3934f176a210e21
LouisBanvillet <Louis@130.66.204.223>
2012-10-12 16:40:20 +0200

    Modif

--- src/traitementcontrolepresence/TraitementControlePresence.java
+++ src/traitementcontrolepresence/TraitementControlePresence.java
@@ -5,6 +5,8 @@
 package traitementcontrolepresence;
 
 import Frame.TraitementControlePresenceFrame;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 /**
  *
@@ -16,8 +18,25 @@ public class TraitementControlePresence {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        try {
+                Class.forName("org.postgresql.Driver");
+                System.out.println("DRIVER OK ! ");
+
+                String url = "jdbc:postgresql://localhost:5432/AGAP_PGROU";
+                String user = "postgres";
+                String passwd = "sagara";
+
+                Connection conn = DriverManager.getConnection(url, user, passwd);
+                System.out.println("Connection effective !");			
+
+        } catch (Exception e) {
+                e.printStackTrace();
+        }
+        
         // TODO code application logic here
         TraitementControlePresenceFrame TraitementFrame = new TraitementControlePresenceFrame();
         TraitementFrame.setVisible(true);
     }
+   
 }
